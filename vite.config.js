7ac4f4c08be19ad27029f9435a8e80ab14716b1e
simply decarbonisation1 <sam.simplydecarbonisation1@gmail.com>
2025-10-27 01:02:19 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/simply-decarbonisation/',
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.cjs'
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
